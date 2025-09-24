880a3641f95625254670cf2180ee485a9d61018f
Savan Paradava <savanparadava.212cs027@nitk.edu.in>
2025-09-24 19:02:40 +0530

    autos crud first version

--- mysite/autos/urls.py
+++ mysite/autos/urls.py
@@ -2,14 +2,17 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from . import views
 
+app_name = 'autos'
+
 urlpatterns = [
-    path("",views.AutoListView.as_view()),
-    path("/lookup",views.MakeListView.as_view()),
+    path("",views.AutoListView.as_view(),name="auto-list"),
+    path("main/create/",views.AutoCreateView.as_view(),name="auto-create"),
+    path("main/<int:pk>/update/",views.AutoUpdateView.as_view(),name="auto-update"),
+    path("main/<int:pk>/delete/",views.AutoDeleteView.as_view(),name="auto-delete"),
+
+    path("lookup/",views.MakeListView.as_view(),name="make-list"),
+    path("lookup/create/",views.MakeCreateView.as_view(),name="make-create"),
+    path("lookup/<int:pk>/update/",views.MakeUpdateView.as_view(),name="make-update"),
+    path("lookup/<int:pk>/delete/",views.MakeDeleteView.as_view(),name="make-delete"),
     # path('', TemplateView.as_view(template_name='home/main.html')),
-    path("/lookup/create",views.MakeCreateView.as_view()),
-    path("/lookup/<int:make_id>/update",views.MakeUpdateView.as_view()),
-    path("/lookup/<int:make_id>/delete",views.MakeDeleteView.as_view()),
-    path("/main/create",views.AutoCreateView.as_view()),
-    path("/main/<int:auto_id>/update",views.AutoUpdateView.as_view()),
-    path("/main/<int:auto_id>/delete",views.AutoDeleteView.as_view()),
 ]
--- mysite/autos/views.py
+++ mysite/autos/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from autos.models import Auto, Make
 
 # Create your views here.
+class AutoListView(LoginRequiredMixin,ListView):
+    model = Auto
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["make_count"] = Make.objects.count()
+        return context
+
+class AutoCreateView(LoginRequiredMixin,CreateView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('autos:auto-list')
+
+class AutoUpdateView(LoginRequiredMixin,UpdateView):
+    model = Auto
+    fields = '__all__'
+    # template_name_suffix = "_update_form"
+    success_url = reverse_lazy('autos:auto-list')
+
+class AutoDeleteView(LoginRequiredMixin,DeleteView):
+    model = Auto
+    success_url = reverse_lazy('autos:auto-list')
+
+
+
+
+class MakeListView(LoginRequiredMixin,ListView):
+    model = Make
+
+class MakeCreateView(LoginRequiredMixin,CreateView):
+    model = Make
+    fields = '__all__'
+    success_url = reverse_lazy('autos:auto-list')
+
+class MakeUpdateView(LoginRequiredMixin,UpdateView):
+    model = Make
+    fields = '__all__'
+    # template_name_suffix = "_update_form"
+    success_url = reverse_lazy('autos:auto-list')
+
+class MakeDeleteView(LoginRequiredMixin,DeleteView):
+    model = Make
+    success_url = reverse_lazy('autos:auto-list')
