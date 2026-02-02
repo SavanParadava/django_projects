4d31a07a52f33383a073408d09f2783c5c5a9e25
Savan Paradava <savanparadava.212cs027@nitk.edu.in>
2026-02-02 19:49:01 +0530

    Final

--- ecommerce/users/managers.py
+++ ecommerce/users/managers.py
@@ -3,14 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
+
     def create_user(self, username, password, email, **extra_fields):
-        """
-        Create and save a user with the given email and password.
-        """
+        
         if not (email or extra_fields.get("is_superuser",None)):
             raise ValueError(_("The Email must be set"))
         
@@ -22,9 +17,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, password, email=None, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
+
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
